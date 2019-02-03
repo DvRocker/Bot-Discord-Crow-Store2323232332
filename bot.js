43144cba3e228df75753e05fbfdd1906fbb5c4eb
DvRocker <42909244+DvRocker@users.noreply.github.com>
2019-02-03 14:57:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,17 +3,8 @@ const client = new Discord.Client();
 var prefix = "!"
 
 
-client.on('guildMemberAdd', member => {
-    var embed = new Discord.RichEmbed()
-    .setThumbnail(member.user.avatarURL)
-  .addField("***شكرا الانضمامك الينا***" ,member.user.username )
-    .setDescription('***بكل حب واحترام وشوق نستقبلك ونتمنى لك قضآء أجمل اللحظات ولآوقات معنا***')
-    .setColor('RANDOM')
-    .setImage('http://www.imgion.com/images/01/Welcome-buddy.jpg')
-var channel =member.guild.channels.find('name', 'chat')
-if (!channel) return;
-channel.send({embed : embed});
-});
+
+
 
 
 client.on('message', message => {
