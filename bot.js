0351e468667057d2d633dc39655dc40262f99add
DvRocker <42909244+DvRocker@users.noreply.github.com>
2019-02-05 13:29:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,20 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 var prefix = "!"
 
+client.on('guildMemberAdd', member => {
+    var embed = new Discord.RichEmbed()
+    .setThumbnail(member.user.avatarURL)
+  .addField("***شكرا الانضمامك الينا***" ,member.user.username )
+    .setDescription('***بكل حب واحترام وشوق نستقبلك ونتمنى لك قضآء أجمل اللحظات ولآوقات معنا***')
+    .setColor('RANDOM')
+    .setImage('http://www.imgion.com/images/01/Welcome-buddy.jpg')
+var channel =member.guild.channels.find('name', 'welcome')
+if (!channel) return; // Toxic Code
+channel.send({embed : embed});
+}); // n3k4a
+
+
+
 client.on('guildMemberAdd', member=> {
     member.addRole(member.guild.roles.find("name","- CS 195 -"));
     });
